8260caceb5ad7e6bae1888321bc3c1c5a11ac5c0
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-02-06 17:31:35 +0600

    responcive degain add

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -4,7 +4,28 @@ const Navbar = () => {
   return (
     <div>
       <div className="bg-slate-800">
-        <ul className="flex justify-center gap-16 text-white py-6 font-serif items-center text-lg">
+        <ul className=" md:flex justify-center gap-16 text-white py-6 font-serif items-center text-lg hidden">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Portfolio</a>
+          </li>
+          <li>
+            <a href="#">Skill</a>
+          </li>
+          <li>
+            <a href="#">Contacet</a>
+          </li>
+        </ul>
+      </div>
+
+
+      <div className="bg-slate-800">
+        <ul className=" flex flex-col justify-center gap-4 text-white py-4 font-serif items-center text-lg block md:hidden">
           <li>
             <a href="#">Home</a>
           </li>
