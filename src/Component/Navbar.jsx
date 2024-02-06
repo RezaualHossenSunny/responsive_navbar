3582c293fddea1bc7406b60c70ee0921f8536c5a
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-02-06 17:41:09 +0600

    icon add

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -23,6 +23,22 @@ const Navbar = () => {
         </ul>
       </div>
 
+      <div className="bg-slate-800 text-white flex justify-end pr-3 block md:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2.5}
+          stroke="currentColor"
+          className="w-8 h-7"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </div>
 
       <div className="bg-slate-800">
         <ul className=" flex flex-col justify-center gap-4 text-white py-4 font-serif items-center text-lg block md:hidden">
